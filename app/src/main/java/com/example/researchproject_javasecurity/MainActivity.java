9d387b80b96d55c17a89aedd2a899596a74ec2a0
Maya Cole <colemayal96@gmail.com>
2021-12-04 10:19:33 -0800

    I think I finally set up AppAuth Library..

--- app/src/main/java/com/example/researchproject_javasecurity/MainActivity.java
+++ app/src/main/java/com/example/researchproject_javasecurity/MainActivity.java
@@ -4,14 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
+
+
 import com.example.researchproject_javasecurity.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
+    private int RC_AUTH=100;
 
     private TextView mTextView;
     private ActivityMainBinding binding;
 
     //Client ID: 20049550827-qbsvqno3n8rp6tnl6fpr6b1h160dufst.apps.googleusercontent.com
+    //API key: AIzaSyAN5zBlUbpLEze3mtqvVgcyvxO6ORz9JWk
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
